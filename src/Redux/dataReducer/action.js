865afb315c4a49c8aa6e0d4e1edfeaa80aeafcd5
shashankfeb16 <shashankfeb16@gmail.com>
2022-11-05 20:25:12 +0530

    infinite scrolling dong

--- src/Redux/dataReducer/action.js
+++ src/Redux/dataReducer/action.js
@@ -2,10 +2,10 @@ import { DATA_ERROR, DATA_LOADING, DATA_SUCCESS } from "./action.types";
 
 import axios from "axios";
 
-export const getData = () => (dispatch) => {
+export const getData = (page) => (dispatch) => {
   dispatch({ type: DATA_LOADING });
   axios
-    .get("https://randomuser.me/api/?results=500")
+    .get(`https://randomuser.me/api/?page=${page}&results=50`)
     .then((res) => {
       dispatch({ type: DATA_SUCCESS, payload: res.data.results });
     })
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,23 +3,38 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getData } from "../Redux/dataReducer/action";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useState } from "react";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [data,setData] = useState([]);
+  const [page, setPage] = useState(1);
   const dispatch = useDispatch();
 
-  const { results } = useSelector((state) => state.data);
+  // const { results } = useSelector((state) => state.data);
 
   // console.log(results)
-
+  const fetchData = ()=>{
+    axios.get(`https://randomuser.me/api/?page=${page}&results=50`).then((res)=>{
+      setData([...data,...res.data.results])
+    })
+    // dispatch(getData(page));
+    // setData(results);
+    setTimeout(()=>{
+      setPage(page+1)
+    },1000)
+  }
   useEffect(() => {
-    dispatch(getData());
+    fetchData()
   }, []);
 
-  console.log(results);
+  console.log(data)
   return (
     <Box>
       <Heading>Dashboard</Heading>
-      {results.map((el) => {
+      <InfiniteScroll dataLength={data.length} next={fetchData}  hasMore={true}  loader={<Heading>Loading...</Heading>}>
+      {data.map((el) => {
         return (
           <Box>
             <Text>
@@ -32,9 +47,12 @@ const Dashboard = () => {
               src={el.picture.medium}
               alt="Dan Abramov"
             ></Image>
+            
           </Box>
         );
       })}
+      </InfiniteScroll>
+      
     </Box>
   );
 };
